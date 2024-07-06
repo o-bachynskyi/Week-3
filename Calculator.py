660a17a5c30e86681a6027b2bf63392e0fdc2c8c
o-bachynskyi <138506032+o-bachynskyi@users.noreply.github.com>
2024-07-06 17:39:03 +0300

    Added Python files and GitHub Actions directory and required files

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
-def add(a,b):
+def subtract(a,b):
     return a - b
 
 if __name__ == "__main__":
-    result = add(2, 1)
+    result = subtract(2, 1)
     print(f"The result is {result}")
     
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
 import unittest
-from main import add
+from Calculator import subtract
 
 class TestMain(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 1), 1)
-        self.assertEqual(add(-1, 1), -2)
-        self.assertEqual(add(0, 0), 0)
+    def test_subtraction(self):
+        self.assertEqual(subtract(2, 1), 1)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(0, 0), 0)
